a9f770ec260d1e7085c84119958084e5e4d0b77b
Kartik <kartikkharche99@gmail.com>
2023-08-24 19:24:38 +0530

    Generic Stack

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+
+  /*      Stack<Integer> s = new Stack<Integer>(3);
+        s.push(4);
+        s.push(2);
+        s.push(3);
+        s.display();
+
+        System.out.println(s.pop());*/
+
+
+   /*     Stack<String> st = new Stack<String>(2);
+        st.push("KARTIK");
+        st.push("kartik");
+      //  System.out.println(st.pop());
+        System.out.println("--------------------------------------------");
+
+        st.pop();
+
+        st.display();*/
+
+        /*Stack<Integer> stack = new Stack<Integer>(4);
+        stack.push(1);
+        stack.push(2);
+        stack.pop();
+        //stack.push(3);
+        stack.pop();
+        //stack.display();*/
+
+        Stack<String> StackString = new Stack(3);
+        StackString.push("AA");
+        StackString.push("BB");
+        StackString.push("CC");
+        StackString.push("DD");
+        StackString.display();
+    }
+}
